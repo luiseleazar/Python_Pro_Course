ab36e66793abaf9a31025a4362db66d04971ce4f
Luis Eleazar N <eleanv05@gmail.com>
2022-02-12 18:57:55 -0700

    Added user interaction to generator2.py

--- generator2.py
+++ generator2.py
@@ -11,7 +11,8 @@ def fibu_gen(max):
     
 
 def run ():
-    fibu = fibu_gen(10) #Instancing generator
+    max = int(input("Enter how many numbers of fibunacci series you want to be display: "))
+    fibu = fibu_gen(max) #Instancing generator
     for element in fibu:
         print(element)
         time.sleep(0.5) #Waits 0.5 second
